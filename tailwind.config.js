1045ba3ee44525dcb1b69a29eb46ac83524563f4
Kyle Kent <kyle.kent@ingka.ikea.com>
2025-05-06 22:32:14 -0400

    change tailwind config from ts to js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'selector',
+};
